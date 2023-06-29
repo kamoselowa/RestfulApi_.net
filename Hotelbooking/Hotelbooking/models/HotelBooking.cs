219ce1b92f5596d1cfc121dc2213cc95c598bf80
kamoselowa <89394456+kamoselowa@users.noreply.github.com>
2023-06-30 01:18:46 +0200

    Add files via upload

---
+++ Hotelbooking/Hotelbooking/models/HotelBooking.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+
+namespace HotelbookingAPI.models
+{
+    public class HotelBooking
+    {
+        public int Id {get; set;}   
+        public int RoomNumber { get; set;}
+        public string ClientName { get; set;}
+    }
+}
